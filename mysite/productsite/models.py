456f318e29ac030d400a237a18c053b1843a3256
fy-i05 <fatimayounus05@gmail.com>
2024-09-10 00:27:48 -0500

    added category sorting and deal ratings

--- mysite/productsite/models.py
+++ mysite/productsite/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 
-# Create your models here
-
-class Products(models.Model):
-    name = models.TextField(blank=True, null=True)
-    price = models.TextField(blank=True, null=True)
-    categories = models.TextField(blank=True, null=True)
-    image = models.URLField()
-    link = models.URLField()
-
-    class Meta:
-        managed = False
-        db_table = 'products'
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
 
-    class Meta:
-        unique_together = ('name', 'parent',)  #makes unique subcategories within each category
-        verbose_name_plural = "categories"
+    def __str__(self):
+        return self.name
+        
+    
+
+class Products(models.Model):
+    name = models.TextField(blank=True, null=True)
+    price = models.TextField(blank=True, null=True)
+    original_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)#to find the deal rating
+    current_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)# to find deal rating
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, related_name='products')
+    image = models.URLField()
+    link = models.URLField()
 
     def __str__(self):
         return self.name
+
+    def get_deal_rating(self):
+        if self.original_price and self.current_price:
+            discount = (self.original_price - self.current_price) / self.original_price * 100
+            if discount < 10:
+                return "Low"
+            elif discount < 20:
+                return "Medium"
+            else:
+                return "High"
+        return "No discount"
--- mysite/productsite/urls.py
+++ mysite/productsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 from productsite.views import product_view
+from .views import products_by_category
 
 urlpatterns= [
     path('', views.product_view, name='product_home'), #root url
+    path('api/products_by_category/', products_by_category, name='products_by_category'),
 ]
